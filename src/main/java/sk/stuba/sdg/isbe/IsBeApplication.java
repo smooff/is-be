5e80f356a1e0c934cf8c7db08e002b7fb33b7071
smooff <smo.offchannel@gmail.com>
2023-02-27 01:20:15 +0100

    init

---
+++ src/main/java/sk/stuba/sdg/isbe/IsBeApplication.java
@@ -0,0 +1,13 @@
+package sk.stuba.sdg.isbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/stuba/sdg/isbe/IsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.stuba.sdg.isbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
