49b2fe63b0db5ce3b2254f3aeae4d57996a8f4ff
smooff <smo.offchannel@gmail.com>
2023-04-05 00:09:52 +0200

    fix: added profiles for tests
    - should fix jenkins build errors

--- src/test/java/sk/stuba/sdg/isbe/IsBeApplicationTests.java
+++ src/test/java/sk/stuba/sdg/isbe/IsBeApplicationTests.java
@@ -2,8 +2,10 @@ package sk.stuba.sdg.isbe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = {"test"})
 class IsBeApplicationTests {
 
 	@Test
