bc3d27a8a78356cc1acdab21ac08b256850cb942
smooff <smo.offchannel@gmail.com>
2023-04-05 18:12:47 +0200

    feat: web config - disable CORS

---
+++ src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package sk.stuba.sdg.isbe.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+public class WebConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        return http.build();
+    }
+}
