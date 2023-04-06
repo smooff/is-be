b56bd6895ec61f4453b2ea3930f176e3c053eed4
Michal Rendek <rendekm.michal@gmail.com>
2023-04-06 15:07:35 +0200

    fix: set https and API key auth

--- src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
+++ src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
@@ -1,19 +1,49 @@
 package sk.stuba.sdg.isbe.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class WebConfig{
 
+    @Value("${sdg.http.auth-token-header-name}")
+    private String principalRequestHeader;
+
+    @Value("${sdg.http.auth-token}")
+    private String principalRequestValue;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+        filter.setAuthenticationManager(authentication -> {
+            String principal = (String) authentication.getPrincipal();
+            if (!principalRequestValue.equals(principal))
+            {
+                throw new BadCredentialsException("The API key was not found or not the expected value.");
+            }
+            authentication.setAuthenticated(true);
+            return authentication;
+        });
         // resolves 403 forbidden - when POST curl (DB insert) triggers
         http.csrf().disable();
-//        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+
+        // use the API key like authenticate method, comment all below for see swagger and connect ot endpoints
+        // without authentication
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+//                and().addFilter(filter).authorizeHttpRequests().
+//                requestMatchers("/api/device").permitAll().
+//                anyRequest().authenticated();
+
+    // http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         return http.build();
     }
 }
