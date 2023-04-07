f5ce9a6b465617dc752c9bbd716f510aa98f5824
Michal Rendek <rendekm.michal@gmail.com>
2023-04-07 22:56:40 +0200

    fix: deployment properties

--- src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
+++ src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
