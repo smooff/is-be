490b8fb7616cb5cde50a0d2cc185951978ceab4f
xbudinsky96 <budinsky12@gmail.com>
2023-04-12 16:40:04 +0200

    feat: add SortingUtils methods

--- src/main/java/sk/stuba/sdg/isbe/utilities/SortingUtils.java
+++ src/main/java/sk/stuba/sdg/isbe/utilities/SortingUtils.java
@@ -1,7 +1,15 @@
 package sk.stuba.sdg.isbe.utilities;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import sk.stuba.sdg.isbe.domain.enums.SortDirectionEnum;
+import sk.stuba.sdg.isbe.handlers.exceptions.InvalidOperationException;
 import sk.stuba.sdg.isbe.handlers.exceptions.NotFoundCustomException;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
 
 public final class SortingUtils {
 
@@ -9,7 +17,42 @@ public final class SortingUtils {
         try {
             return Enum.valueOf(SortDirectionEnum.class, sortDirection.toUpperCase());
         } catch (IllegalArgumentException e) {
-            throw new NotFoundCustomException("Sort direction: '" + sortDirection + "' does not exist! Possible options are ASC or DESC!");
+            throw new NotFoundCustomException("Sort direction: '" + sortDirection + "' does not exist! Possible options are ASC, DESC or NONE!");
         }
+    }
+
+    public static Sort getDirectedSorting(Sort sorting, String sortDirection) {
+        SortDirectionEnum sortDirectionEnum = getSortDirection(sortDirection);
+        if (SortDirectionEnum.NONE == sortDirectionEnum) {
+            return sorting;
+        }
+        return SortDirectionEnum.ASC == sortDirectionEnum ? sorting.ascending() : sorting.descending();
+    }
+
+    public static String getValidSortingField(String sortBy, Class<?> klass) {
+        List<String> recipeSortingFields = Arrays.stream(klass.getDeclaredFields()).map(Field::getName).toList();
+        for (String field : recipeSortingFields) {
+            if (field.equalsIgnoreCase(sortBy)) {
+                return field;
+            }
+        }
+        throw new NotFoundCustomException("Sorting field: " + sortBy + " can't be found in " + klass.getName() + " class!" +
+                " Possible sorting fields: " + String.join(", ", recipeSortingFields));
+    }
+
+    public static Pageable getPagination(Class<?> klass, String sortBy, String sortDirection, int page, int pageSize) {
+        if (page <= 0) {
+            throw new InvalidOperationException("Page must be greater than 0!");
+        }
+        if (pageSize <= 0) {
+            throw new InvalidOperationException("Size of the page must be greater than 0!");
+        }
+
+        if (SortDirectionEnum.NONE == getSortDirection(sortDirection)) {
+            return PageRequest.of(page - 1, pageSize);
+        }
+
+        Sort sorting = getDirectedSorting(Sort.by(getValidSortingField(sortBy, klass)), sortDirection);
+        return PageRequest.of(page - 1, pageSize, sorting);
     }
 }
