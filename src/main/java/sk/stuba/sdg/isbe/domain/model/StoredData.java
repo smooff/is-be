b9a00d026368a8251a2473aca11681e9c243d6f3
smooff <smo.offchannel@gmail.com>
2023-04-18 01:37:18 +0200

    fix: cleanup

--- src/main/java/sk/stuba/sdg/isbe/domain/model/StoredData.java
+++ src/main/java/sk/stuba/sdg/isbe/domain/model/StoredData.java
@@ -1,7 +1,6 @@
 package sk.stuba.sdg.isbe.domain.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 
 public class StoredData {
     @Id
