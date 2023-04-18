46ed91d5f83cb30fe6dbea6c3809ad54c785340f
smooff <smo.offchannel@gmail.com>
2023-04-18 18:13:34 +0200

    fix: TEMP fix not working https

--- src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
+++ src/main/java/sk/stuba/sdg/isbe/configuration/WebConfig.java
@@ -31,24 +31,24 @@ public class WebConfig{
         // resolves 403 forbidden - when POST curl (DB insert) triggers
         http.csrf().disable();
 
-        if(environment.getActiveProfiles().equals("deployment")){
-            APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
-            filter.setAuthenticationManager(authentication -> {
-                String principal = (String) authentication.getPrincipal();
-                if (!principalRequestValue.equals(principal))
-                {
-                    throw new BadCredentialsException("The API key was not found or not the expected value.");
-                }
-                authentication.setAuthenticated(true);
-                return authentication;
-            });
-            // use the API key like authenticate method, comment all below for see swagger and connect ot endpoints
-            // without authentication
-            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-                and().addFilter(filter).authorizeHttpRequests().
-                requestMatchers("/api/device").permitAll().
-                anyRequest().authenticated();
-        }
+//        if(environment.getActiveProfiles().equals("deployment")){
+//            APIKeyAuthFilter filter = new APIKeyAuthFilter(principalRequestHeader);
+//            filter.setAuthenticationManager(authentication -> {
+//                String principal = (String) authentication.getPrincipal();
+//                if (!principalRequestValue.equals(principal))
+//                {
+//                    throw new BadCredentialsException("The API key was not found or not the expected value.");
+//                }
+//                authentication.setAuthenticated(true);
+//                return authentication;
+//            });
+//            // use the API key like authenticate method, comment all below for see swagger and connect ot endpoints
+//            // without authentication
+//            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+//                and().addFilter(filter).authorizeHttpRequests().
+//                requestMatchers("/api/device").permitAll().
+//                anyRequest().authenticated();
+//        }
 
         // http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
         return http.build();
