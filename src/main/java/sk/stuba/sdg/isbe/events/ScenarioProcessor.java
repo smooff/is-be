5c2c0d865aed8fe4a1155e273dbad7525f6ad51e
smooff <smo.offchannel@gmail.com>
2023-05-17 20:01:45 +0200

    perf: cleanup after perf vol2

--- src/main/java/sk/stuba/sdg/isbe/events/ScenarioProcessor.java
+++ src/main/java/sk/stuba/sdg/isbe/events/ScenarioProcessor.java
@@ -85,10 +85,9 @@ public class ScenarioProcessor {
                                             if (k.equals(event.getDeviceId()) && tag.equals(storedData.getTag())) {
                                                 // we can skip one DB call, because we already put actual storedData to dataForExpression
                                             } else {
-                                                StoredData lastStoredData = storedDataRepository.findFirstStoredDataByDeviceIdAndTagOrderByMeasureAddDesc(k, tag);
-//                                                LastStoredData lastStoredData1 = lastStoredDataRepository.findByDeviceIdAndTag(k, tag);
-                                                if (lastStoredData != null) {
-                                                    dataForExpression.put(lastStoredData.getDeviceId() + lastStoredData.getTag(), lastStoredData.getValue());
+                                                LastStoredData lastStoredData1 = lastStoredDataRepository.findByDeviceIdAndTag(k, tag);
+                                                if (lastStoredData1 != null) {
+                                                    dataForExpression.put(lastStoredData1.getDeviceId() + lastStoredData1.getTag(), lastStoredData1.getValue());
                                                 } else {
                                                     hasAllDataForEvaluation.set(false);
                                                 }
